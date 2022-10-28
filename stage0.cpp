9a4f8f6d8d1881973be21c09da324e745939f82e
Esai <95305895+EsaiN7@users.noreply.github.com>
2022-10-28 11:30:40 -0500

    Friday Oct 28 2022
    Beginning of compiler project, stage0

--- stage0.cpp
+++ stage0.cpp
@@ -1,1 +1,23 @@
+//Esai Barron and Victor Obioma
+//CS 4301
+//Stage 0
 
+#ifndef STAGE0_H
+#define STAGE0_H
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+const char END_OF_FILE = '$'; 
+
+enum storeTypes {INTEGER, BOOLEAN, PROG_NAME};
+enum modes {VARIABLE, CONSTANT};
+enum allocation {YES, NO};
+
+class SymbolTableEntry{
+
+
+}
