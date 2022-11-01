e7bb84469db1238ae31f83401f57cc540b7899ec
Victor Obioma <112091274+vobioma34@users.noreply.github.com>
2022-11-01 13:28:38 -0500

    Update stage0.cpp
    I add ALL the functions within the SymbolTableEntry class in the public field and the variables in the private field. Next is the compiler class bro.

--- stage0.cpp
+++ stage0.cpp
@@ -11,13 +11,72 @@
 
 using namespace std;
 
-const char END_OF_FILE = '$'; 
+const char END_OF_FILE = '$'; // arbitrary choice
 
 enum storeTypes {INTEGER, BOOLEAN, PROG_NAME};
 enum modes {VARIABLE, CONSTANT};
 enum allocation {YES, NO};
 
-class SymbolTableEntry{
-
-
-}
+class SymbolTableEntry 
+{
+public:
+  // Public functions
+  SymbolTableEntry(string in, storeTypes st, modes m,
+                   string v, allocation v, int u)
+  {
+    setInternalName(in);
+    setDataType(st);
+    setValue(v);
+    setAlloc(a);
+    setUnits(u);
+  }
+  string getInternalName() const {
+    return internalName;
+  }
+  storeTypes getDataType() const {
+    return dataType;
+  }
+  modes getMode() const {
+    return mode;
+  }
+  string getValue() const {
+    return value;
+  }
+  allocation getAlloc() const {
+    return alloc;
+  }
+  int getUnits() const {
+    return units;
+  }
+  void setInternalName(string s) {
+    internalName = s;
+  }
+  void setDataType(storeTypes st) {
+    dataType = st;
+  }
+  void setMode(modes m) {
+    mode = m;
+  }
+  void setValue(string s) {
+    value = s;
+  }
+  void setAlloc(allocation a) {
+    alloc = a;
+  }
+  void setUnits(int i) {
+    units = i;
+  }
+  // End of the public field
+private:
+  // Private members
+  string internalName;
+  storeTypes dataType;
+  modes mode;
+  string value;
+  allocation alloc;
+  int units;
+  // End of the private field
+};
+// Editor: Victor Obioma
+// edited 11/1/2022 1:26pm
+// Next is the creation of the Compiler class below...
