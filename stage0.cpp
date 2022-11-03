09cee0b02ef1b3cba180308cf302f922bab8a3ba
Esai <95305895+EsaiN7@users.noreply.github.com>
2022-11-02 19:38:06 -0500

    Change made by Esai 11/2/2022 at 7:37pm

--- stage0.cpp
+++ stage0.cpp
@@ -2,11 +2,31 @@
 //CS 4301
 //Stage 0
 
-Compiler(char **argv) { // constructor
-	/*
-	open sourceFile using argv[1]
- 	open listingFile using argv[2]
- 	open objectFile using argv[3]
-	*/
+#include <stage0.h>
+#include <iostream>
+
+using namespace std;
+
+//constructor
+Compiler(char **argv) {
+    //open source file using argv[1]
+    sourceFile.open(argv[1]);
+    //open listing file using argv[2]
+    listingFile.open(argv[2]);
+    //open objectFile using argv[3]
+    objectFile.open(argv[3]);
+
 }
-// edited on 11/1/2022 at 10:35pm by Victor Obioma
+
+//destructor
+~Compiler(){
+    sourceFile.close();
+    listingFile.close();
+    objectFile.close();
+}
+
+void createListingHeader(){
+
+    //GET CODE FROM VICTOR/GITHUB
+}
+
