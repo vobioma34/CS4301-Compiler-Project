45e03a693e6fe7c33e3746eb7e2fff86ddc2e4df
Esai <95305895+EsaiN7@users.noreply.github.com>
2022-11-02 21:45:14 -0500

    Esai 11/2/2022 9:44pm
    fixed output to listing file, and implemented processError() function

--- stage0.cpp
+++ stage0.cpp
@@ -1,47 +1,51 @@
 //Esai Barron and Victor Obioma
 //CS 4301
 //Stage 0
+
 #include <stage0.h>
 #include <iostream>
 #include <ctime>
+#include <string>
 
 using namespace std;
 
-Compiler::Compiler(char **argv) 
-{
-   /*
-   open sourceFile using argv[1]
-   open listingFile using argv[2]
-   open objectFile using argv[3]
-   */
+//constructor
+Compiler::Compiler(char **argv) {
     //open source file using argv[1]
     sourceFile.open(argv[1]);
     //open listing file using argv[2]
     listingFile.open(argv[2]);
     //open objectFile using argv[3]
     objectFile.open(argv[3]);
+
 }
-Compiler::~Compiler()
-{
-	// close all open files
-	sourceFile.close();
+
+//destructor
+Compiler::~Compiler(){
+    sourceFile.close();
     listingFile.close();
     objectFile.close();
 }
-void Compiler::createListingHeader()
-{
-	time_t result = time(nullptr);
-	cout << "STAGE0:" << "Esai Baron and Victor Obioma" << ctime(&result) << endl;
-	cout << "LINE NO:" << "SOURCE STATEMENT" << endl;
+
+void Compiler::createListingHeader(){
+    time_t result = time(nullptr);
+	listingFile << "STAGE0:" << "Esai Baron and Victor Obioma " << ctime(&result) << endl;
+	listingFile << "LINE NO:" << "SOURCE STATEMENT" << endl;
 }
-// "the new int main()"
-void Compiler::parser()
-{
-	
+
+//"new int main() - Womack 2022"
+void Compiler::parser() {
+    //cout << "sussy balls" << endl;
 }
-void Compiler::createListingTrailer()
-{
-	cout << "COMPILATION TERMINATED" << "# ERRORS ENCOUNTERED" << endl;
+
+void Compiler::createListingTrailer(){
+    listingFile << "COMPILATION TERMINATED" << "# ERRORS ENCOUNTERED" << endl;
+}
+
+void Compiler:: processError(string err) {
+    //output error to listing file and call exit()
+    listingFile << err;
+    exit(0);
 }
 
 char Compiler :: nextChar(){
@@ -57,5 +61,12 @@ char Compiler :: nextChar(){
     listingFile << ch;
     return ch;
 }
+
+string Compiler :: nextToken(){
+
+    //GET CODE FROM VICTOR/GITHUB
+    return "default";
+}
+
 
 
